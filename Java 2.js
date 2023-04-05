294cfba8fc4fd173c1a09aa8683336a0a81ff090
guilt98 <124409817+guilt98@users.noreply.github.com>
2023-04-04 21:24:16 -0300

    Add files via upload

---
+++ Java 2.js
@@ -0,0 +1,11 @@
+const USER_ADMIN = { name:'Erick ramos',
+login: 'root'
+};
+USER_ADMIN.login= 'admin';
+console.log(USER_ADMIN);
+var s1 = parseint(34);
+var s2 = parsefloat(15.6);
+
+var s3 = s1 + s2;
+
+console.log(soma);
